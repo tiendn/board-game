7a5f01860a973ddf0de2953f76dc36d2096ff6d8
Tiendn <daonamtien1@gmail.com>
2019-03-14 22:38:12 +0700

    Updates

---
+++ precache-manifest.9a97b6e7c8055a430a51635160588918.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8669933a5cdc89a0ce12",
+    "url": "/board-game/static/css/main.39aefa6f.chunk.css"
+  },
+  {
+    "revision": "8669933a5cdc89a0ce12",
+    "url": "/board-game/static/js/main.b940d6fa.chunk.js"
+  },
+  {
+    "revision": "d56f4495e5210cf57a96",
+    "url": "/board-game/static/js/runtime~main.854404e4.js"
+  },
+  {
+    "revision": "262c54fec9d3bcafc40d",
+    "url": "/board-game/static/js/2.6e7329be.chunk.js"
+  },
+  {
+    "revision": "384ce530a0050411bf42102fdc03cca9",
+    "url": "/board-game/static/media/card-default.384ce530.png"
+  },
+  {
+    "revision": "2162441eda092135a7e41fabf9f23881",
+    "url": "/board-game/static/media/lion.2162441e.png"
+  },
+  {
+    "revision": "102321232d359afa8a10228d96fea878",
+    "url": "/board-game/static/media/background1.10232123.png"
+  },
+  {
+    "revision": "c1a0f31989fc5966da96bf2df27266bf",
+    "url": "/board-game/static/media/background.c1a0f319.jpg"
+  },
+  {
+    "revision": "55333c2160283f1a1251858a8d18f7db",
+    "url": "/board-game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/board-game/precache-manifest.22581c82db8fbf82c92ddf38d1aa8e42.js"
+  "/board-game/precache-manifest.9a97b6e7c8055a430a51635160588918.js"
 );
 
 workbox.clientsClaim();
